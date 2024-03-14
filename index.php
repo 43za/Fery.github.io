7b3e3f1b01531753de0ea136718f20e3c475876c
43za <160724469+43za@users.noreply.github.com>
2024-03-15 06:14:43 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    $nama = "Fery";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nama Fery dengan Warna Hijau</title>
+</head>
+<body>
+    <p>Halo, nama saya <span style="color: green;"><?php echo $nama; ?></span>.</p>
+</body>
+</html>
